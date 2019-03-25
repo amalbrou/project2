8a7e6d5ceeb892ddea5bfb3cc27d332340962338
David Olczak <dolczak@asu.edu>
2019-03-25 15:38:35 -0700

    posts are now displayed on the user page when the page is loaded

---
+++ project2/PostInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project2
+{
+    public class PostInfo
+    {
+        public int fid;
+        public string problem;
+        public string solution;
+    }
+}
